93a99f12d8e5b293a30b57de5d8da974c41dc9d9
Ryotaro Uehara <ryohandball10@gmail.com>
2022-09-16 11:31:44 +0900

    filableの利用

--- src/app/Article.php
+++ src/app/Article.php
@@ -8,6 +8,10 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Article extends Model
 {
+    protected $fillable = [
+        'title',
+        'body',
+    ];
     // use HasFactory;
     public function user(): BelongsTo
     {
