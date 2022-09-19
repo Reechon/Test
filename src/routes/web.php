26194ead6e2ea8855ca374667bd88da471370e51
Ryotaro Uehara <ryohandball10@gmail.com>
2022-09-19 09:24:41 +0900

    ルーティングの確認と編集

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,7 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 Auth::routes();
 Route::get('/','App\Http\Controllers\ArticleController@index')->name('articles.index');
-Route::resource('/articles', 'App\Http\Controllers\ArticleController')->except(['index'])->middleware('auth');
+Route::resource('/articles', 'App\Http\Controllers\ArticleController')->except(['index', 'show'])->middleware('auth');
+Route::resource('/articles', 'App\Http\Controllers\ArticleController')->only(['show']); //-- この行を追加
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
